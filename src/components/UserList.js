fc5afafe0e105e5788602f545a94e9f9c843a815
irwankilay <57196785+irwankilay@users.noreply.github.com>
2022-09-04 01:19:03 +0800

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -33,7 +33,7 @@ const UserList = () => {
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
-        <Banner title="Users List" />
+        <Banner title="Users List - Staging" />
         <Link to={`add`} className="button is-info">
           Add New
         </Link>
